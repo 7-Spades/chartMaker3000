9fa1a2cb31455204162c4f16dc57a33f8e7ee5e0
7-Spades <edwin5106@gmail.com>
2020-06-19 22:57:55 -0500

    added router links

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {Router, Route} from 'react-router-dom';
 import DataContextProvider from './util/dataContext';
+import ThemeContextProvider from './util/themeContext';
+
 function App() {
   return (
     <DataContextProvider>
-   <Router>
-    
-   </Router>
+      <ThemeContextProvider>
+        <Router>
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </Router>
+    </ThemeContextProvider>
    </DataContextProvider>
   );
 }
