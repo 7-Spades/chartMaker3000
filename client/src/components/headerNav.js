37debe13eef56a0b1688cf8950ed7d62d6d99dae
7-Spades <edwin5106@gmail.com>
2020-06-19 22:58:15 -0500

    page chart changer

---
+++ client/src/components/headerNav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function headerNav(){
+    return(
+        <nav>
+            <ul class="nav nav-pills nav-fill">
+                <li class="nav-item">
+                    <a class={} href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class={} href="#">Much longer nav link</a>
+                </li>
+                <li class="nav-item">
+                    <a class={} href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class={} href="#">Disabled</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default headerNav
